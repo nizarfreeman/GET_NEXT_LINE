4d964669c713d2c29e9138c51522f1477a641ce8
nizarfreeman <110555272+nizarfreeman@users.noreply.github.com>
2024-12-15 01:33:12 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: nfaska <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 15:31:56 by nfaska            #+#    #+#             */
-/*   Updated: 2024/12/13 21:40:35 by nfaska           ###   ########.fr       */
+/*   Updated: 2024/12/15 01:29:56 by nfaska           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_H
@@ -21,6 +21,7 @@
 size_t	ft_strlen(char *s);
 int		search(char *s);
 char	*ft_strcpy(char *dst, char *src);
+void	ft_strncpy(char *dst, char *src, size_t n);
 char	*ft_strjoin(char *s1, char *s2);
 void	copy(char **p, char *s);
 
