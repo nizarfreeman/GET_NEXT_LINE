efa3cedbf0c44bc4d8e45309c8af3a30b357e0d7
nizarfreeman <110555272+nizarfreeman@users.noreply.github.com>
2024-12-15 23:14:52 +0100

    nf

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: nfaska <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 20:31:13 by nfaska            #+#    #+#             */
-/*   Updated: 2024/12/14 01:41:03 by nfaska           ###   ########.fr       */
+/*   Updated: 2024/12/15 22:59:44 by nfaska           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_BONUS_H
@@ -17,7 +17,9 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 4
 # endif
-# define MAX_INT 2147483647
+# ifndef OPEN_MAX
+#  define OPEN_MAX 1024
+# endif
 
 size_t	ft_strlen(char *s);
 int		search(char *s);
@@ -25,5 +27,4 @@ char	*ft_strcpy(char *dst, char *src);
 char	*ft_strjoin(char *s1, char *s2);
 void	copy(char **p, char *s);
 
-char	*get_next_line(int fd);
 #endif
