aff2b988a8ae46e578078cb4258aabd164c4e853
nizarfreeman <110555272+nizarfreeman@users.noreply.github.com>
2024-12-17 15:23:22 +0100

    nf

--- get_next_line.h
+++ get_next_line.h
@@ -3,17 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nfaska <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: nfaska <nfaska@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 15:31:56 by nfaska            #+#    #+#             */
-/*   Updated: 2024/12/15 19:24:01 by nfaska           ###   ########.fr       */
+/*   Updated: 2024/12/17 01:31:00 by nfaska           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 4
 # endif
@@ -21,8 +23,9 @@
 size_t	ft_strlen(char *s);
 int		search(char *s);
 char	*ft_strcpy(char *dst, char *src);
-void	ft_strncpy(char *dst, char *src, size_t n);
 char	*ft_strjoin(char *s1, char *s2);
 void	copy(char **p, char *s);
+
+char	*get_next_line(int fd);
 
 #endif
