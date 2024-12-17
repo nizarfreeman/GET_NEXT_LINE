1fe9ff8dccb61ec45d9316a7440141850d4ed70b
nizarfreeman <110555272+nizarfreeman@users.noreply.github.com>
2024-12-17 22:02:45 +0100

    nf

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static char	*get_line(int fd, char **line)
 {
